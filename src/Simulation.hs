54f3cccbfc4f0a83321076fd4dc2fb5d7ff2bc2c
CimimUxMaio <cimimuxmaio@gmail.com>
2020-10-04 16:09:33 -0300

    Subo una version mas acorde, mas prolija y en espaniol

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -1,120 +1,48 @@
-module Simulation 
+module Simulation
 where
-    import Text.Show.Functions 
-    import Data.List
+    import System.Random
 
-    data DataVars = DataVars { metersRequested :: Meter, timeBetweenRequests :: Time }
-    data SimulationState = SimulationState { actualTime :: Time, tc :: Time, acum :: Meter, nextRequest :: Time } deriving Eq 
-    data SimulationResults = SimulationResults { r1 :: Time } deriving (Show, Eq)
-    type Time = Float
-    type Meter = Int
-
-
--- Getters and setters
-
-    setTC :: Time -> SimulationState -> SimulationState 
-    setTC value currentState = currentState { tc = value }
-
-    setAcum :: Meter -> SimulationState -> SimulationState
-    setAcum value currentState = currentState { acum = value }
-
-    addTC :: Time -> SimulationState -> SimulationState
-    addTC value currentState = setTC (tc currentState + value) currentState
-
-    setActualTime :: Time -> SimulationState -> SimulationState 
-    setActualTime value currentState = currentState { actualTime = value }
-
-    addActualTime :: Time -> SimulationState -> SimulationState 
-    addActualTime value currentState = setActualTime (actualTime currentState + value) currentState
-
-    setNextRequest :: Time -> SimulationState -> SimulationState
-    setNextRequest value currentState = currentState { nextRequest = value }
-
-    addNextRequest :: Time -> SimulationState -> SimulationState
-    addNextRequest delta currentState = setNextRequest (nextRequest currentState + delta) currentState
-
-
--- Config
-
-    furnanceCapacity :: Meter
-    furnanceCapacity = 1600
-
-    timePerCicle :: Time
-    timePerCicle = 6
-
-    initialState :: SimulationState
-    initialState = SimulationState { tc = 0, actualTime = 0, acum = 0, nextRequest = 0 }
-
-    startEvent :: SimulationState -> SimulationState
-    startEvent currentState = setActualTime (nextRequest currentState) currentState
-
-
--- Helpers
-
-    ciclesNeeded :: Meter -> Int
-    ciclesNeeded = ceiling . (/ fromIntegral furnanceCapacity) . fromIntegral 
-
-    mayWait :: Meter -> SimulationState -> SimulationState
-    mayWait amountRequested currentState = conditional . setTC (time + timePerCicle) $ currentState
-        where excess = amountRequested - furnanceCapacity
-              time = actualTime currentState
-              conditional = if excess > 0 then wait excess else dontWait
-
-    dontWait :: SimulationState -> SimulationState
-    dontWait = setAcum 0
-
-    wait :: Meter -> SimulationState -> SimulationState
-    wait excess = (\s -> addTC (fromIntegral . ciclesNeeded . acum $ s) s) . setAcum excess 
-
-    simulationStep :: DataVars -> SimulationState -> SimulationState
-    simulationStep dataVars currentState = run . addNextRequest (timeBetweenRequests dataVars) . startEvent $ currentState
-        where run s
-                | actualTime s > tc s = mayWait (metersRequested dataVars) s
-                | otherwise = wait (acum currentState + metersRequested dataVars) s
-
-    fromFinalState :: SimulationState -> SimulationResults
-    fromFinalState finalState = SimulationResults { r1 = actualTime finalState } -- TODO
-
-
--- Logging
-
-    fieldsAsList :: SimulationState -> [String]
-    fieldsAsList aState = map ($ aState) [show . actualTime, show . tc, show . acum, show . nextRequest]
-
-    toTableRegister :: SimulationState -> String
-    toTableRegister = wrap wall . intercalate "\t\t" . padLast "\t   " . fieldsAsList
-        where wall = "|| "
-              wrap wrapper xs = wrapper ++ xs ++ wrapper
-              padFirst padding (s:ss) = (s ++ padding) : ss 
-              padLast padding = reverse . padFirst padding . reverse
-
-    logState :: SimulationState -> IO () 
-    logState currentState = putStrLn $ toTableRegister currentState
-
-    logTable :: IO ()
-    logTable = do
-        let separator = replicate 61 '='
-        putStrLn separator
-        putStrLn "|| TIME\t\tTC\t\tACUM\t\tNEXT EVENT ||"
-        putStrLn separator
+    data EstadoSimulacion = EstadoSimulacion { dia :: Dias }
+    data ResultadoSimulacion = ResultadoSimulacion { clientesArrepentidos :: Porcentaje
+                                                   , tiempoOciosoDiassTrabajados :: Porcentaje
+                                                   , cantidadDeMetrossPromedio :: Metros 
+                                                   } deriving Show
+    type Metros = Int
+    type Dias = Int
+    type Porcentaje = Float
+    type Simulacion = Dias -> EstadoSimulacion -> IO ResultadoSimulacion
 
 
 -- IO
 
-    deltaRequestTime :: IO Time
-    deltaRequestTime = return 5
+    pedidosGenerados :: IO Int
+    pedidosGenerados = randomRIO (3, 10) -- TODO
 
-    deltaMetersRequested :: IO Meter
-    deltaMetersRequested = return 1000
+    cantidadDeZocaloPedida :: IO Metros
+    cantidadDeZocaloPedida = randomRIO (500, 20000) -- TODO
 
--- main
+    checkPorcentaje :: Int -> IO Bool
+    checkPorcentaje porcentaje = do
+        numeroRandom <- randomRIO (0, 100)
+        return (porcentaje > numeroRandom)
 
-    simulation :: Time -> SimulationState -> IO SimulationResults
-    simulation duration = (logTable >>) . run
-        where run currentState
-                | actualTime currentState > duration = return . fromFinalState $ currentState
-                | otherwise = do requestInterval <- deltaRequestTime
-                                 amountRequested <- deltaMetersRequested
-                                 logState currentState
-                                 let dataVars = DataVars { metersRequested = amountRequested, timeBetweenRequests = requestInterval }
-                                 run . simulationStep dataVars $ currentState
+    arrepentimiento :: Dias -> IO Bool
+    arrepentimiento diasDeEspera
+        | diasDeEspera > 15 = return True
+        | diasDeEspera > 10 = checkPorcentaje 60
+        | diasDeEspera > 5  = checkPorcentaje 20
+
+    pasoDeLaSimulacion :: Int -> Int -> EstadoSimulacion -> IO EstadoSimulacion 
+    pasoDeLaSimulacion = undefined
+
+    simulacion :: Int -> Int -> Simulacion
+    simulacion cantidadDeHornos ciclosPorDia duracion = run
+        where run estadoActual
+                | dia estadoActual > duracion = return (calcularResultados estadoActual)
+                | otherwise                   = pasoDeLaSimulacion cantidadDeHornos ciclosPorDia estadoActual >>= run
+
+
+-- Funciones puras
+
+    calcularResultados :: EstadoSimulacion -> ResultadoSimulacion
+    calcularResultados estadoFinal = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,40 +3,6 @@ import Simulation
 
 main :: IO ()
 main = hspec $ do
-   describe "setTC/2:" $ do
-       it "setTC 10 to a state with tc = 0 should return a state with tc = 10" $ do
-           (tc $ setTC 10 initialState) `shouldBe` 10 
-       it "setTC 0 to a state with tc = 0 should return a state with tc = 0" $ do
-           (tc $ setTC 0 initialState) `shouldBe` 0
-       it "setTC should not return a state with diferent fields than the original other than tc" $ do
-           setTC 0 initialState `shouldBe` initialState
-
-   describe "setAcum/2:" $ do
-       it "setAcum 10 to a state with acum = 0 should return a state with acum = 10" $ do
-           (acum $ setAcum 10 initialState) `shouldBe` 10 
-       it "setAcum 0 to a state with acum = 0 should return a state with acum = 0" $ do
-           (acum $ setAcum 0 initialState) `shouldBe` 0
-       it "setAcum should not return a state with diferent fields than the original other than acum" $ do
-           setAcum 0 initialState `shouldBe` initialState
-
-   describe "setActualTime/2" $ do
-       it "setActualTime 10 to a state with acum = 0 should return a state with actualTime = 10" $ do
-           (actualTime $ setActualTime 10 initialState) `shouldBe` 10 
-       it "setActualTime 0 to a state with acum = 0 should return a state with actualTime = 0" $ do
-           (actualTime $ setActualTime 0 initialState) `shouldBe` 0
-       it "setActualTime should not return a state with diferent fields than the original other than actualTime" $ do
-           setActualTime 0 initialState `shouldBe` initialState
-
-   describe "setNextRequest/2" $ do
-       it "setNextRequest 10 to a state with nextRequest = 0 should return a state with nextRequest = 10" $ do
-           (nextRequest $ setNextRequest 10 initialState) `shouldBe` 10 
-       it "setNextRequest 0 to a state with acum = 0 should return a state with nextRequest = 0" $ do
-           (nextRequest $ setNextRequest 0 initialState) `shouldBe` 0
-       it "setNextRequest should not return a state with diferent fields than the original other than nextRequest" $ do
-           setNextRequest 0 initialState `shouldBe` initialState
-
-   describe "addTC/2" $ do
-       it "addTC 10 to a state with tc = 10 should return a state with tc = 20" $ do
-           (tc $ addTC 10 (initialState { tc = 10 })) `shouldBe` 20
-       it "addTC 0 to a state with tc = 0 should return an identical state" $ do
-           addTC 0 initialState `shouldBe` initialState
+   describe "subgrupo de ejemplo" $ do
+       it "descripcion de ejemplo" $ do
+           10 `shouldBe` 10 
